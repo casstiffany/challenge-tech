c6829dc229cea58528b1a28976d34ac6779af277
casstiffany <casstiffany.villeronce@gmail.com>
2019-10-18 15:24:24 +0200

    basic homepage & basic form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import logo from './logo-epsor-normal.svg';
+import Challenge from './Challenge';
+import './style/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+class App extends React.Component {
+
+  render(){
+
+    return (
+    <Router>
+      <div className="App">
+          <Switch>
+              <Route path ="/" exact component ={Home} />
+              <Route path ="/challenge" component={Challenge} />
+          </Switch>
+      </div>
+     </Router>
+
+    
   );
+
+  }
+ 
 }
+
+const Home = () => (
+     <header className="">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Challenge Tech par Cassandra-Tiffany
+          </p>
+          <Link to='/challenge'>
+              <button className="">Découvrir</button>
+          </Link>
+      </header>
+);
 
 export default App;
---
+++ src/Challenge.js
@@ -0,0 +1,69 @@
+import React from 'react';
+
+
+
+class Challenge extends React.Component {
+
+
+  render(){
+
+
+    return (
+
+ 
+    	<form>
+      			<div>
+  
+      					<p>Quelle est votre meilleure adresse e-mail ?</p>
+      					<input 
+      						className ="" 
+      						type="email"
+      						name="email" 
+      						placeholder = "contact@amoureuxduvoyage.com"
+      					/>
+                <div className="errorMessage"></div>
+                </div>
+
+      			<div id ="q2">
+      					<p>Quel est le super nom de votre entreprise ?</p>
+      					<input 
+      						className ="" 
+      						type="text" 
+      						name="name"
+                   			id="name"
+      					/> 
+
+               <div className="errorMessage"></div>
+            </div>
+      			<div id="q3" >
+      					<p>Combien de salariés compte votre entreprise ?</p>
+      					<input 
+      						className ="" 
+      						type="number"
+      						name="employee" 
+                  			id="employee"
+      					/> 
+                <div className="errorMessage"></div>
+            </div>
+      			<div id="q4">
+      					<p>Qui est votre prestataire actuel ?</p>
+      					<input 
+      						className ="" 
+      						type="text" 
+      						name="competitor"
+                  			id="competitor"
+      						placeholder = "Nom du prestataire"
+      					/>  
+                <div className="errorMessage"></div>
+      			</div>
+        </form>
+
+        )}
+      
+
+
+  
+ 
+}
+
+export default Challenge;
